c6aedc09322b64c0b6e073277a03e34ffba04f10
sabater20 <joaquingsabater@gmail.com>
2026-08-28 13:28:57 -0300

    arreglos

--- backend/src/insumos/insumos.module.ts
+++ backend/src/insumos/insumos.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InsumosService } from './insumos.service';
 import { InsumosController } from './insumos.controller';
+import { OrganizationsModule } from '../organizations/organizations.module';
 
 @Module({
+  imports: [OrganizationsModule],
   providers: [InsumosService],
   controllers: [InsumosController],
 })
